c38ec1668229c7109bd705da882ffd9c4e3d96d6
Sunny Nahar <sunnyn@cmu.edu>
2014-07-10 23:59:41 -0400

    Added .crx compilation batch file

---
+++ update.bat
@@ -0,0 +1,1 @@
+C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --pack-extension=C:\Users\Sunny Nahar\Desktop\Computer Science\Chrome Extensions\YouTubeTracker --pack-extension-key=C:\Users\Sunny Nahar\Desktop\Computer Science\Chrome Extensions\YouTubeTracker.pem
