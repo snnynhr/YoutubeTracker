8e58f243cf2a1333c8f6d9f93924c768c7b0f7a3
Sunny Nahar <sunnyn@cmu.edu>
2014-12-22 21:32:04 -0500

    moved scripts

---
+++ compile/zip.bat
@@ -0,0 +1,4 @@
+CD ..
+DEL VideoTracker.zip
+CD YouTubeTracker
+"C:\Program Files\7-Zip\7z.exe" a "../VideoTracker.zip" icon/ skin/popup.png skin/fonts/ skin/*.min.css util/*.min.js js/*.min.js background.html manifest.json options.html popup.html VideoTracker.crx
