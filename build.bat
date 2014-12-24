dec977e570e449663e122c7bff09edb31f807186
Sunny Nahar <sunnyn@cmu.edu>
2014-12-24 09:02:04 -0500

    build script

--- build.bat
+++ build.bat
@@ -1,12 +1,22 @@
-rmdir build
+rmdir build /S /Q
 mkdir build
 CD build/
 mkdir icon
 mkdir js
 mkdir skin
 mkdir util
-COPY icon/*.png build/icon
-::call minifycss.bat
-::call minifyjs.bat
-::call update_crx.bat
-::call zip.bat
+CD ..
+XCOPY icon build\icon /Y
+DEL build\icon\icon.xcf
+XCOPY util\jquery.min.js build\util
+XCOPY skin\fonts build\skin\fonts /I
+XCOPY skin\popup.png build\skin
+XCOPY background.html build
+XCOPY manifest.json build
+XCOPY options.html build
+XCOPY popup.html build
+XCOPY README.md build
+call compile\minifycss.bat
+call compile\minifyjs.bat
+call compile\update_crx.bat
+call compile\zip.bat
